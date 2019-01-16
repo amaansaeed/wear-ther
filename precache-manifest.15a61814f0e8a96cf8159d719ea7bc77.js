dd39d284f4472f423fc21c745a2ac093c66a5f95
Amaan Saeed <amaansaeed@Guava.local>
2019-01-16 16:16:45 -0500

    Updates

---
+++ precache-manifest.15a61814f0e8a96cf8159d719ea7bc77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61e2dee6f9cd19193225",
+    "url": "/wear-ther/static/css/main.880f2d67.chunk.css"
+  },
+  {
+    "revision": "61e2dee6f9cd19193225",
+    "url": "/wear-ther/static/js/main.61e2dee6.chunk.js"
+  },
+  {
+    "revision": "4238da98fc3cc11a757f",
+    "url": "/wear-ther/static/css/1.28801e6b.chunk.css"
+  },
+  {
+    "revision": "4238da98fc3cc11a757f",
+    "url": "/wear-ther/static/js/1.4238da98.chunk.js"
+  },
+  {
+    "revision": "4ec24a2781e39fdf905b",
+    "url": "/wear-ther/static/js/runtime~main.4ec24a27.js"
+  },
+  {
+    "revision": "d494dd852643cf3d4b8d713447937015",
+    "url": "/wear-ther/index.html"
+  }
+];
