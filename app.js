3dcade49a5edd821467f70d23c72cad145c14037
amaan <17208409+amaansaeed@users.noreply.github.com>
2019-02-08 10:07:06 -0500

    adds server favicon support

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
 //  Dependencies
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const helmet = require("helmet");
-const path = require("path");
-require("dotenv").config();
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const path = require('path')
+require('dotenv').config()
 
 // //  Initialize Smart Home
 // require("./gpio/intitalize")
 
 //  Routes
-const routes = require("./routes/api_v1_routes");
+const routes = require('./routes/api_v1_routes')
 
 //  Express
-const app = express();
+const app = express()
 
 //  Port & Listener
-app.set("port", process.env.PORT);
+app.set('port', process.env.PORT)
 
-app.listen(app.get("port"), () =>
-  console.log(`'Wearther' server listening on port: ${app.get("port")}`)
-);
+app.listen(app.get('port'), () =>
+  console.log(`'Wearther' server listening on port: ${app.get('port')}`)
+)
 
 //  Middleware
-app.use(express.json()); // to support JSON-encoded bodies
-app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
-app.use(morgan("tiny")); // request logger
-app.use(helmet()); //  third-party middleware
-app.use(cors());
+app.use(express.json()) // to support JSON-encoded bodies
+app.use(express.urlencoded({ extended: true })) // to support URL-encoded bodies
+app.use(morgan('tiny')) // request logger
+app.use(helmet()) //  third-party middleware
+app.use(cors())
 
 //  API Routing
-app.use("/api/v1", routes);
+app.use('/api/v1', routes)
 
 // Serve the static files from the React app
 app.use(
-  "/static",
-  express.static(path.join(__dirname, "./client/build/static"))
-);
-app.get("*", function(req, res) {
-  res.sendFile("index.html", { root: path.join(__dirname, "./client/build/") });
-});
+  '/static',
+  express.static(path.join(__dirname, './client/build/static'))
+)
+app.get('/favicon.ico', function(req, res) {
+  res.sendFile('favicon.ico', { root: path.join(__dirname, './client/build/') })
+})
+app.get('*', function(req, res) {
+  res.sendFile('index.html', { root: path.join(__dirname, './client/build/') })
+})
